6248baafe501ab7ed0aed2ffcb315dc6f72d29b3
agates4 <agate97@gmail.com>
2017-05-12 19:26:47 -0400

    more progress

--- ncursesPac-master/src/main.cpp
+++ ncursesPac-master/src/main.cpp
@@ -3,87 +3,122 @@
 #include <Game/CStateMenu.h>
 
 // the database stuff
+#include <stdio.h>
 #include <stdlib.h>
+#include <sqlite3.h>
 #include <iostream>
+#include <ctime>
+#include <ratio>
+#include <chrono>
+#include <algorithm>
+#include <fstream>
 #include <string>
-#include <sstream>
-#include "mysql_connection.h"
-#include <cppconn/driver.h>
-#include <cppconn/exception.h>
-#include <cppconn/resultset.h>
-#include <cppconn/statement.h>
-#include <cppconn/prepared_statement.h>
 
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    sql::Driver *driver;
-    sql::Connection *con;
-    driver = get_driver_instance();
-    con = driver->connect("dbdev.cs.kent.edu", "agates10", "suLs74sD");
-    con->setSchema("agates10");
-    sql::PreparedStatement *pstmt = con->prepareStatement("SELECT COUNT(ID) FROM `user` WHERE `username` = aron AND `password` = gates");
-    sql::ResultSet *res = pstmt->executeQuery();
-    while (res->next()) {
-        std::cout << "You Entered: " << res->getInt("ID") << endl;
+    sqlite3 *db;
+    sqlite3_stmt * stmt;
+    /* Open database */
+    if( sqlite3_open("database.sqlite", &db) ){
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return(0);
+    }else{
+        // success
     }
-    delete pstmt;
-    delete res;
-    delete con;
     
+    char mesg[]="Do you need to register? [y/n] ";		/* message to be appeared on the screen */
+    char str[80];
+    int row,col;                                        /* to store the number of rows and */
+                                                        /* the number of colums of the screen */
+    initscr();                                          /* start the curses mode */
+    getmaxyx(stdscr,row,col);                           /* get the number of rows and columns */
+    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);     /* print the message at the center of the screen */
+    getstr(str);
+    mvprintw(LINES - 2, 0, "You Entered: %s", str);
     
-//    char mesg[]="Do you need to register? [y/n] ";		/* message to be appeared on the screen */
-//    char str[80];
-//    int row,col;                                        /* to store the number of rows and */
-//                                                        /* the number of colums of the screen */
-//    initscr();                                          /* start the curses mode */
-//    getmaxyx(stdscr,row,col);                           /* get the number of rows and columns */
-//    mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);     /* print the message at the center of the screen */
-//    getstr(str);
-//    mvprintw(LINES - 2, 0, "You Entered: %s", str);
-//    
-//    std::string whiteOut = "";
-//    for (int i = 0; i < col-strlen(mesg) + col-strlen(str); ++i) {
-//        whiteOut += " ";
-//    }
-//    mvprintw(row/2,(col-strlen(mesg))/2,"%s",whiteOut.c_str());
-//    if(str[0] == 'n' && str[1] == '\0') {
-//        mvprintw(row/2 - 2,(col-strlen("Login"))/2,"%s","Login");
-//        mvprintw(row/2,(col-strlen("Username: "))/2,"%s","Username: ");
-//        getstr(str);
-//        mvprintw(LINES - 2, 0, "You Entered: %s", str);
-//        std::string username = str;
-//        mvprintw(row/2 + 1,(col-strlen("Password: "))/2,"%s","Password: ");
-//        getstr(str);
-//        std::string password = str;
-//        mvprintw(LINES - 2, 0, "You Entered: %s", str);
-//        
-//        sql::Driver *driver;
-//        sql::Connection *con;
-//        driver = get_driver_instance();
-//        con = driver->connect("dbdev.cs.kent.edu", "agates10", "suLs74sD");
-//        con->setSchema("agates10");
-//        sql::PreparedStatement *pstmt = con->prepareStatement("SELECT ID FROM `user` WHERE `username` = " + username + " AND `password` = " + password);
-//        sql::ResultSet *res = pstmt->executeQuery();
-//        while (res->next()) {
-//            mvprintw(LINES - 2, 0, "You Entered: %s", res->getInt("ID"));
-//        }
-//        delete pstmt;
-//        delete res;
-//        delete con;
-//    }
-//    else {
-//        mvprintw(row/2,(col-strlen("dong farts"))/2,"%s","dong farts");
-//    }
-//    getch();
-//    
-//    endwin();
-//    
-//    CNCurses::init();
-//    CStateManager states;
-//    states . run ( new CStateMenu );
-//    CNCurses::exit();
-//    
-//    return 0;
+    string whiteOut = "";
+    for (int i = 0; i < col-strlen(mesg) + col-strlen(str); ++i) {
+        whiteOut += " ";
+    }
+    mvprintw(row/2,(col-strlen(mesg))/2,"%s",whiteOut.c_str());
+    if(str[0] == 'n' && str[1] == '\0') {
+        mvprintw(row/2 - 2,(col-strlen("Login"))/2,"%s","Login");
+        mvprintw(row/2,(col-strlen("Username: "))/2,"%s","Username: ");
+        getstr(str);
+        mvprintw(LINES - 2, 0, "You Entered: %s", str);
+        string username = str;
+        mvprintw(row/2 + 1,(col-strlen("Password: "))/2,"%s","Password: ");
+        getstr(str);
+        string password = str;
+        mvprintw(LINES - 2, 0, "You Entered: %s", str);
+        
+        string query = "SELECT COUNT(ID) FROM user WHERE username = '" + username + "' AND password = '" + password + "'";
+        bool validUser = false;
+        if ( sqlite3_prepare(db, query.c_str(), -1, &stmt, NULL ) == SQLITE_OK )
+        {
+            int res = 0;
+            while ( 1 )
+            {
+                res = sqlite3_step(stmt);
+                if ( res == SQLITE_ROW ) {
+                    string s = (char*)sqlite3_column_text(stmt, 0);
+                    if (s == "1")
+                        validUser = true;
+                }
+                if ( res == SQLITE_DONE || res==SQLITE_ERROR)
+                    break;
+            }
+        }
+        if (validUser) {
+            mvprintw(LINES - 2, 0, "Login successful: %s", "press any button to continue.");
+        }
+        else {
+            mvprintw(LINES - 2, 0, "Login failed: %s", "this program will terminate.");
+            getch();
+            endwin();
+            return 0;
+        }
+
+    }
+    else {
+        mvprintw(row/2,(col-strlen("Bad input. This program will terminate. "))/2,"%s","Bad input. This program will terminate. ");
+        getch();
+        endwin();
+        return 0;
+    }
+    getch();
+    endwin();
+    
+    CNCurses::init();
+    CStateManager states;
+    states . run ( new CStateMenu );
+    CNCurses::exit();
+    
+    sqlite3_close(db);
+    
+    return 0;
 }
+
+
+
+
+//string query = "SELECT ID FROM user WHERE username = 'aron'";
+//bool validUser = false;
+//if ( sqlite3_prepare(db, query.c_str(), -1, &stmt, NULL ) == SQLITE_OK )
+//{
+//    validUser = true;
+//    int ctotal = sqlite3_column_count(stmt);
+//    int res = 0;
+//    
+//    while ( 1 )
+//    {
+//        res = sqlite3_step(stmt);
+//        if ( res == SQLITE_ROW )
+//            string s = (char*)sqlite3_column_text(stmt, 0);
+//        cout << s << endl;
+//        if ( res == SQLITE_DONE || res==SQLITE_ERROR)
+//            break;
+//    }
+//}
